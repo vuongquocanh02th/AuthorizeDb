2867de75a7c351698243db09844ff81ca24a937f
Vuong Quoc Anh <vuongquocanh300402@gmail.com>
2025-02-04 17:07:01 +0700

    add password encode in config

---
+++ src/main/java/com/authorizedb/config/SecurityInit.java
@@ -0,0 +1,7 @@
+package com.authorizedb.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer{
+
+}
