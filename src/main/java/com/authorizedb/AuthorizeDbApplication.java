01ab3b9f7e4a9ed90435bf4e28a0d7d527bac53c
Vuong Quoc Anh <vuongquocanh300402@gmail.com>
2025-02-04 16:47:35 +0700

    init

---
+++ src/main/java/com/authorizedb/AuthorizeDbApplication.java
@@ -0,0 +1,13 @@
+package com.authorizedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizeDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizeDbApplication.class, args);
+    }
+
+}
