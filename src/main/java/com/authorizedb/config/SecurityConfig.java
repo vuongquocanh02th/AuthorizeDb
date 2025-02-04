b60f06f932cc4714cabf6b6b6854298b0b79167d
Vuong Quoc Anh <vuongquocanh300402@gmail.com>
2025-02-04 17:20:48 +0700

    inmemory

--- src/main/java/com/authorizedb/config/SecurityConfig.java
+++ src/main/java/com/authorizedb/config/SecurityConfig.java
@@ -1,17 +1,35 @@
 package com.authorizedb.config;
 
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     //xac thuc
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        //builder pattern
+        UserDetails user = User.withUsername("PhongEm").password(passwordEncoder()
+                        .encode("123456"))
+                        .roles("USER").build();
+        UserDetails admin = User.withUsername("Phong").password(passwordEncoder()
+                        .encode("123456"))
+                        .roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
