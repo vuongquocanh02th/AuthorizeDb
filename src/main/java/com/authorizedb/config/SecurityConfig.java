fdc3581446d46816e45bff656b269f487b7bb2b6
Vuong Quoc Anh <vuongquocanh300402@gmail.com>
2025-02-04 21:42:31 +0700

    secure filter chain

--- src/main/java/com/authorizedb/config/SecurityConfig.java
+++ src/main/java/com/authorizedb/config/SecurityConfig.java
@@ -4,13 +4,17 @@ import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -31,5 +35,14 @@ public class SecurityConfig {
                         .encode("123456"))
                         .roles("ADMIN").build();
         return new InMemoryUserDetailsManager(user, admin);
+    }
+    //Phan quyen
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(
+                        author -> author.anyRequest().authenticated()
+                ).csrf(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 }
